df53180a490d32e0ae636ac469b4275cb482cffb
LizValdez <liza_vv@hotmail.com>
2015-07-30 19:37:36 -0300

    Taxis

---
+++ PFinal/Mibuspuntano/Models/TaxiRemises.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mibuspuntano.Models
+{
+    public class TaxiRemises
+    {
+        int desde { get; set; }
+        int hasta { get; set; }
+        int tarifa { get; set; }
+    
+
+    public int recorrido(int desde, int hasta)
+        {
+
+                    return tarifa;
+        }
+    }
+}
