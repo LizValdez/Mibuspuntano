065ddb1cd4ddbb3f0e571ea526cce20ae31067f6
LizValdez <liza_vv@hotmail.com>
2015-07-30 20:35:02 -0300

    Apie

---
+++ PFinal/Mibuspuntano/Models/Apie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mibuspuntano.Models
+{
+    public class APie
+    {
+        int desde { get; set; }
+        int hasta { get; set; }
+    }
+}
